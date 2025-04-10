79c366df50ec8bfd10badbc85929d35ac7d0fae7
Marco Marinho <marcoobom@gmail.com>
2025-04-10 19:04:14 +0200

    Day 25

--- lib/days/day25.ex
+++ lib/days/day25.ex
@@ -1,25 +1,24 @@
 defmodule AOC2024.Day25 do
-  def is_hash?(c) do
-    case c do
-      "#" -> 1
-      _ -> 0
-    end
-  end
-
   def count_line(line) do
-    Enum.map(line, fn c -> is_hash?(c) end) |> List.to_tuple()
+    Enum.map(line, fn c -> if c == "#", do: 1, else: 0 end)
   end
 
-  def sum_lines({a, b, c, d, e}, {f, g, h, i, j}) do
-    {a + f, b + g, c + h, d + i, e + j}
+  def sum_lines(l1, l2) do
+    Enum.zip(l1, l2) |> Enum.map(fn {a, b} -> a + b end)
   end
 
-  def fits?({a, b, c, d, e}, {f, g, h, i, j}) do
-    if a + f < 8 and b + g < 8 and c + h < 8 and d + i < 8 and e + j < 8 do
-      1
-    else
-      0
-    end
+  def fits?(l1, l2) do
+    Enum.zip(l1, l2) |> Enum.map(fn {a, b} -> a + b end) |> Enum.all?(fn x -> x < 8 end)
+  end
+
+  def parse(block) do
+    block
+    |> Enum.map(fn entry -> Enum.map(entry, fn line -> String.graphemes(line) end) end)
+    |> Enum.map(fn entry ->
+      Enum.reduce(entry, List.duplicate(0, 5), fn line, acc ->
+        count_line(line) |> sum_lines(acc)
+      end)
+    end)
   end
 
   def part1(input) do
@@ -29,25 +28,10 @@ defmodule AOC2024.Day25 do
 
     groups = Enum.group_by(blocks, fn block -> block |> hd |> String.starts_with?("#") end)
 
-    keys =
-      Map.get(groups, false)
-      |> Enum.map(fn block -> Enum.map(block, fn line -> String.graphemes(line) end) end)
+    keys = Map.get(groups, false) |> parse()
+    locks = Map.get(groups, true) |> parse()
 
-    locks =
-      Map.get(groups, true)
-      |> Enum.map(fn block -> Enum.map(block, fn line -> String.graphemes(line) end) end)
-
-    keys_count =
-      Enum.map(keys, fn block ->
-        Enum.reduce(block, {0, 0, 0, 0, 0}, fn line, acc -> count_line(line) |> sum_lines(acc) end)
-      end)
-
-    locks_count =
-      Enum.map(locks, fn block ->
-        Enum.reduce(block, {0, 0, 0, 0, 0}, fn line, acc -> count_line(line) |> sum_lines(acc) end)
-      end)
-
-    fitting = for k <- keys_count, l <- locks_count, do: fits?(k, l)
-    fitting |> Enum.sum() |> IO.inspect(label: "Part 1")
+    fitting = for k <- keys, l <- locks, do: fits?(k, l)
+    fitting |> Enum.count(& &1) |> IO.inspect(label: "Part 1")
   end
 end
